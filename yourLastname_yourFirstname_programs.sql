45ec7b583249a7272584f85f11a295aa91afb960
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2019-10-27 23:45:46 +0300

    100% working with error messages and test data

--- yourLastname_yourFirstname_programs.sql
+++ yourLastname_yourFirstname_programs.sql
@@ -1,5 +1,5 @@
 /**********************************************************
- ****** Stored Programs for Assn.2, 2019 *******************
+ ****** Stored Programs FOR Assn.2, 2019 *******************
  ********** Student Name and Number ************************
  ******************* Date **********************************
  ** I DECLARE that the code provided below is my own work **
@@ -21,19 +21,25 @@ FOR EACH ROW
     END
 //
 
-/************* Helper FUNCTIONs/PROCEDUREs used, two FUNCTIONs for example ****************/
+/************* Helper FUNCTIONs/PROCEDUREs used, two FUNCTIONs FOR example ****************/
 
 DROP FUNCTION IF EXISTS rate_on_date
 //
-CREATE FUNCTION rate_on_date(staff_id int, given_date DATE) returns float DETERMINISTIC
-    BEGIN
-        DECLARE total_days_pay float;
-        DECLARE hour_rate float;
-        DECLARE cur1 CURSOR for select HOURLYRATE,SUM(HOURLYRATE*HOUR) from staffongrade,salarygrade,workson WHERE staffongrade.STAFFNO=staff_id AND salarygrade.GRADE=staffongrade.GRADE AND workson.WDATE=given_date AND workson.STAFFNO=staff_id;
-        open cur1;
-        read_loop: LOOP
-        FETCH cur1 INTO hour_rate,total_days_pay;
-    END LOOP;
+CREATE FUNCTION rate_on_date(staff_id INTEGER, given_date DATE) returns FLOAT DETERMINISTIC
+BEGIN
+    DECLARE total_days_pay FLOAT;
+    DECLARE hour_rate FLOAT;
+    DECLARE finished INTEGER;
+    DECLARE cur1 CURSOR FOR SELECT HOURLYRATE FROM staffongrade,salarygrade,workson WHERE staffongrade.STAFFNO=staff_id AND salarygrade.GRADE=staffongrade.GRADE AND workson.WDATE=given_date AND workson.STAFFNO=staff_id;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished=1;
+    OPEN cur1;
+    read_loop: LOOP
+        IF finished=1 THEN
+            LEAVE read_loop;
+        END IF;
+        FETCH cur1 INTO hour_rate;
+    END LOOP ;
+    CLOSE cur1;
     return hour_rate;
 
 END
@@ -43,22 +49,26 @@ DROP FUNCTION IF EXISTS cost_of_campaign
 
 //
 
-CREATE FUNCTION cost_of_campaign (camp_id int)
-returns float
+CREATE FUNCTION cost_of_campaign (camp_id INTEGER)
+returns FLOAT
 -- retuens the total cost incurred due to any given campaigh (camp_id)
 -- READS SQL DATA
 -- DETERMINISTIC
 BEGIN
     -- code
     DECLARE total_cost FLOAT;
-    DECLARE cur1 CURSOR FOR select SUM(HOURLYRATE*HOUR) from staffongrade,salarygrade,workson,campaign WHERE staffongrade.STAFFNO=workson.STAFFNO AND salarygrade.GRADE=staffongrade.GRADE AND workson.CAMPAIGN_NO=camp_id AND staffongrade.STARTDATE >=campaign.CAMPAIGNSTARTDATE AND staffongrade.FINISHDATE <= campaign.CAMPAIGNFINISHDATE;
-    open cur1;
+    DECLARE finished INTEGER;
+    DECLARE cur1 CURSOR FOR SELECT SUM(HOURLYRATE*HOUR) FROM staffongrade,salarygrade,workson,campaign WHERE staffongrade.STAFFNO=workson.STAFFNO AND salarygrade.GRADE=staffongrade.GRADE AND workson.CAMPAIGN_NO=camp_id AND staffongrade.STARTDATE >=campaign.CAMPAIGNSTARTDATE AND staffongrade.FINISHDATE <= campaign.CAMPAIGNFINISHDATE;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished=1;
+    OPEN cur1;
     read_loop: LOOP
-    FETCH cur1 INTO total_cost;
-END LOOP;
-
--- employ FUNCTION  rate_on_date IF necessary
-return total_cost;
+        IF finished=1 THEN
+            LEAVE read_loop;
+        END IF;
+        FETCH cur1 INTO total_cost;
+    END LOOP ;
+    CLOSE cur1;
+    return total_cost;
 
 END
 //
@@ -73,19 +83,33 @@ CREATE PROCEDURE sp_finish_campaign (IN c_title VARCHAR(30))
 
 -- code
 -- DECLARE CONTINUE HANDLER FOR NOT FOUND; */
--- error_string =  'ERROR! Campaign title does not exist'; */
 BEGIN
     -- do the error handling
-DECLARE cost int;
-DECLARE camp_id int;
-DECLARE error_string varchar(255);
+    DECLARE cost INTEGER;
+    DECLARE finished INTEGER;
+    DECLARE camp_id INTEGER;
+    DECLARE result_count INTEGER;
+    DECLARE error_string varchar(255) DEFAULT "N";
+    DECLARE cs CURSOR FOR SELECT COUNT(CAMPAIGN_NO) FROM campaign WHERE TITLE=c_title;
     DECLARE mycursor CURSOR FOR SELECT CAMPAIGN_NO FROM campaign WHERE TITLE=c_title;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished=1;
+    OPEN cs;
+    FETCH cs INTO result_count;
+    CLOSE cs;
     OPEN mycursor;
     read_loop: LOOP
-    FETCH mycursor INTO camp_id;
-    END LOOP;
-    SET cost = cost_of_campaign(camp_id);
-    UPDATE campaign SET CAMPAIGNFINISHDATE = CURRENT_DATE , ACTUALCOST = cost WHERE TITLE=c_title;
+        IF result_count=0 THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT="ERROR! Campaign title does not exist";
+            LEAVE read_loop;
+        END IF;
+        IF finished=1 THEN
+            LEAVE read_loop;
+        END IF;
+        FETCH mycursor INTO camp_id;
+    END LOOP ;
+    SET cost=cost_of_campaign(camp_id);
+    UPDATE campaign SET CAMPAIGNFINISHDATE=CURRENT_DATE , ACTUALCOST=cost WHERE TITLE=c_title;
+    CLOSE mycursor;
 END
 //
 
--- yourLastname_yourFirstname_test_script.sql
+++ yourLastname_yourFirstname_test_script.sql
@@ -56,6 +56,14 @@ rollback;
 -- The test above is minimal with very little data.
 -- Think of tests that you need to carry out to gain confidence cthat the programs you wrote does the right thing.
 -- Insert more data to the tables strategically to show quickly if there are semantic errors in your programs.
+insert into customer(CUSTOMER_ID,COMPANYNAME,ADDRESS,STAFF_STAFFNO)
+values
+(9,"ABC LTD","Texas",1),
+(5,"NOVATTA LTD","Ohio",5),
+(4,"TRALAHTEK LTD","Home",1),
+(8,"JKL LTD","GUAM",3),
+(7,"XYZ LTD","CHICAGO",5);
+
 insert into campaign (CAMPAIGN_NO,TITLE,CUSTOMER_ID,THEME,CAMPAIGNSTARTDATE,CAMPAIGNFINISHDATE,ESTIMATEDCOST,ACTUALCOST)
 values
 (6,"Marketing",9,"Dummy","2017-04-02","2017-08-09",25500,NULL),
@@ -63,14 +71,38 @@ values
 (9,"campaign",4,"broadcast","2019-10-23","2019-12-29",45700,NULL),
 (8,"roadshow",8,"mysql","2018-04-23","2018-12-23",9500,NULL),
 (10,"reseller",7,"technology","2019-04-13","2019-06-19",19000,NULL);
--- run the tests and display the results.
+
+insert into workson (STAFFNO,CAMPAIGN_NO,WDATE,HOUR)
+values
+(1,6,'2017-04-20',8),
+(3,6,'2017-05-20',6),
+(5,7,'2018-07-20',7),
+(3,7,'2018-07-20',9),
+(1,8,'2018-04-30',8),
+(3,8,'2018-05-06',6),
+(5,8,'2018-07-11',7),
+(1,10,'2019-04-20',5),
+(3,10,'2019-04-20',7);
+
+insert into invoice(CAMPAIGN_NO,DATEISSUED,BALANCEOWING,STATUS)
+values
+(6,'2017-08-09',25500,"UNPAID"),
+(7,'2018-11-29',25900,"UNPAID"),
+(8,'2018-12-23',9500,"PAID"),
+(10,'2019-06-19',19000,"UNPAID");
 commit;
-select * from campaign;
-select * from invoice;
+-- run the tests and display the results.
 call sp_finish_campaign('promotion');
+call sp_finish_campaign('roadshow');
+call sp_finish_campaign('reseller');
 call sp_finish_campaign('Marketing');
+--- check error status
+call sp_finish_campaign('ghadskbbcsjl');
 call sync_invoice;
 select * from alerts;
+select * from campaign;
+/* select * from invoice; */
+commit;
 
 
 -- Finish with commiting work or rolling back.
